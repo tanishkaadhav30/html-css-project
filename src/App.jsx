b9bd8694235f84193737274f2e371c5a9f1d790e
tanishkaadhav30 <tanishkaadhav30@gmail.com>
2026-07-28 22:29:12 +0530

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./views/Home";
 import About from "./views/About";
 import Contact from "./views/Contact";
@@ -8,12 +9,17 @@ import Contact from "./views/Contact";
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="app-shell">
+        <Navbar />
+        <main className="page-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 Simple Store. All rights reserved.</p>
+      <p>Built with React and React Router.</p>
+    </footer>
+  );
+}
+
+export default Footer;
